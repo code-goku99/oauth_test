d1e7b43e1990e76cccf3dacd9f5557af4e753ff8
codegoku <codegoku99@gmail.com>
2020-12-19 13:25:42 +0530

    Photos App

---
+++ Photos/src/main/java/com/photos/PhotosApplication.java
@@ -0,0 +1,14 @@
+package com.photos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosApplication.class, args);
+		
+	}
+
+}
---
+++ Photos/src/main/java/com/photos/PhotosController.java
@@ -0,0 +1,25 @@
+package com.photos;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.photos.model.PhotosResponse;
+
+@RestController
+@RequestMapping("/photos")
+public class PhotosController {
+
+	@GetMapping("/show")
+	public List<PhotosResponse> getAllPhotos(){
+		PhotosResponse photosResponse1 = new PhotosResponse();
+		photosResponse1.setAlbumId("A001");
+		photosResponse1.setPhotoId("PH001");
+		photosResponse1.setPhotoTitle("First");
+		
+		return Arrays.asList(photosResponse1);
+	}
+}
